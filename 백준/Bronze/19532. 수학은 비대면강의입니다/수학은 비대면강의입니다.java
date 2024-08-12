a0903a941469873e51c295d5c2619017c464cea0
DDong_kka <jjsh0208@gmail.com>
2024-08-12 22:02:49 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 112 ms, Memory: 14888 KB -BaekjoonHub

--- 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
@@ -1,30 +1,31 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         
         int[] ary = new int[6];
 
-        for (int i = 0; i < ary.length; i++) {
-            ary[i] = Integer.parseInt(st.nextToken());
-        }
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
+        int f = Integer.parseInt(st.nextToken());
 
-        for (int i = -999; i <= 999; i++) {
-            for (int j = -999; j <= 999; j++) {
-                if ((ary[0] * i) + (ary[1] * j) == ary[2]) {
-                    if ((ary[3] * i) + (ary[4] * j) == ary[5]) {
-                        System.out.println(i +" "+j);
-                        break;
-                    }
-                }
-            }
-        }
+        bw.write((c * e - b * f) / (a * e - b * d)  + " ");
+        bw.write((c * d - a * f) / (b * d - a * e)+ "");
+
+        bw.flush();
+
         br.close();
     }
 }

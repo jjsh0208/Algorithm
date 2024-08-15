3dd2a104859b47917e4bbb10705c9981e42d9e73
DDong_kka <jjsh0208@gmail.com>
2024-08-15 23:43:09 +0900

    [Silver V] Title: 나이순 정렬, Time: 1232 ms, Memory: 61868 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       
+        int n = Integer.parseInt(br.readLine());
+
+        String[][] ary = new String[n][2];
+
+        for (int i = 0; i < ary.length; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine()," ");
+            ary[i][0] = st.nextToken();
+            ary[i][1] = st.nextToken();
+        }
+
+        Arrays.sort(ary , new Comparator<String[]>() {
+
+            @Override
+            public int compare(String[] o1, String[] o2) {
+                return Integer.parseInt(o1[0]) - Integer.parseInt(o2[0]);
+            }
+            
+        });
+
+
+        for (int i = 0; i < ary.length; i++) {
+            System.out.println(ary[i][0] + " " + ary[i][1]);
+        }
+
+        br.close();
+    }
+}

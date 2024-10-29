03197dc485b0742124f189bde983c5911a6a4782
DDong_kka <jjsh0208@gmail.com>
2024-10-29 17:44:55 +0900

    Update 두 개 뽑아서 더하기.java

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -2,6 +2,7 @@ import java.util.TreeSet;
 
 class Solution {
     public int[] solution(int[] numbers) {
+        // TreeSet으로 add 하면서 정렬
         TreeSet<Integer> set = new TreeSet<>();
         
         for(int i = 0; i < numbers.length - 1; i++){
@@ -9,9 +10,7 @@ class Solution {
                 set.add(numbers[i] + numbers[j]);
             }
         }
-    
-    
-        
+        //int[] 배열로 반환해야하기에 mapToInt로 int 타입으로 변환후 배열 반환
         return set.stream().mapToInt(i -> i).toArray();
     }
 }

8a24f243f89968f90a54c13ad6d7b7bd03a3bc07
DDong_kka <jjsh0208@gmail.com>
2024-11-19 22:49:46 +0900

    [level 2] Title: 올바른 괄호, Time: 19.68 ms, Memory: 53.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,23 @@
+import java.util.Stack;
+
+class Solution {
+    boolean solution(String s) {
+        
+        Stack<Character> stack = new Stack<>();
+        
+        char[] chars = s.toCharArray();
+        
+        for(char c : chars){
+            if(c == '('){
+                stack.push(c);
+            }else{
+                if(stack.isEmpty() || stack.pop() == c){
+                    return false;
+                }
+            }
+        }
+        
+    
+        return stack.isEmpty();
+    }
+}

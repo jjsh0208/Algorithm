de89bc9c396fdddc84ea6fa834a84599e501e34c
DDong_kka <jjsh0208@gmail.com>
2024-11-27 11:15:05 +0900

    [level 1] Title: 모의고사, Time: 5.18 ms, Memory: 84.5 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,33 +1,38 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 class Solution {
     public int[] solution(int[] answers) {
         
-        int[][] pattern = {
+        // 수포자들의 패턴
+        int[][] pettern = {
             {1,2,3,4,5},
             {2,1,2,3,2,4,2,5},
-            {3,3,1,1,2,2,4,4,5,5,}
+            {3,3,1,1,2,2,4,4,5,5}
         };
         
-        int[] score = new int[3];
+        // 수포자들의 점수를 저장할 배열포자들의 점수를 저장할 배열
+        int[] scores = new int[3];
         
-        for(int i = 0; i < answers.length; i++){
-            for(int j=0; j< pattern.length; j++){
-                if(answers[i] == pattern[j][i % pattern[j].length]){
-                    score[j]++;
-                }
+        for(int i = 0 ; i < answers.length; i++){
+            for(int j = 0; j < pettern.length; j++){
+                if(answers[i] == pettern[j][ i % pettern[j].length])
+                    scores[j]++;
             }
-        }
+        } 
         
-        int[] result = new int[3];
-        int max = Arrays.stream(score).max().getAsInt();
+        //가장 높은 점수 저장
+        int maxScore = Arrays.stream(scores).max().getAsInt();
         
         ArrayList<Integer> answer = new ArrayList<>();
         
-        for(int i = 0; i < score.length; i++){
-            if(max == score[i]) answer.add(i + 1);
+        for(int i = 0; i < scores.length; i++){
+            if(maxScore == scores[i]){
+                answer.add(i + 1);
+            }
         }
         
-        return answer.stream().mapToInt(i -> i).toArray();
+        
+        return answer.stream().mapToInt(Integer :: intValue).toArray();
     }
 }

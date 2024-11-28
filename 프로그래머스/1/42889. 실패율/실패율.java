b17acfccbb1175a57f7b28a9960f6373b8c8fb54
DDong_kka <jjsh0208@gmail.com>
2024-11-28 10:44:45 +0900

    [level 1] Title: 실패율, Time: 35.96 ms, Memory: 108 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.java
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -2,26 +2,42 @@ import java.util.HashMap;
 
 class Solution {
     public int[] solution(int N, int[] stages) {
+      
+        // 각 스테이지에 도전하는 도전자의 수를 저장하는 배열
+        int[] chellenger = new int[N + 2];
         
-        int[] challenger = new int[N + 2];
-        
+        // 해당 스테이지에 도전하는 도전자의 수를 저장
         for(int i = 0; i < stages.length; i++){
-            challenger[stages[i]] += 1; 
+            chellenger[stages[i]] +=1;
         }
         
-        HashMap<Integer,Double> fials = new HashMap<>();
-
+        // 실패율을 저장할 해시맵 (스테이지 , 실패율)
+        HashMap<Integer, Double> fails = new HashMap<>();
+        
+        // total 사용자의 수
+        // double / int 를 해야 실수로 결과가 나오기때문에 double로 선언
         double total = stages.length;
         
-        for(int i = 1; i <= N; i++){
-            if(challenger[i] == 0){ //도전한 사람이 없는 경우
-                fials.put(i,0.);
+        //스테이지 1 부터 마지막 스테이지 까지 반복
+        for(int i = 1; i <= N;i++){
+            //해당 스테이지의 도전한 사람이 없는 경우 (스테이지 , 0.0)
+            if(chellenger[i] == 0){
+                fails.put(i,0.);
             }
-            else{
-                fials.put(i,challenger[i] / total); //실패율 구하기
-                total -= challenger[i]; //다음 스테이지의 실패율을 구하기위해 현재 스테이지의 인원을 뺌
+            //해당 스테이지의 도전자가 있는 경우 
+            //(스테이지 , 현재 스테이지 도전자 / 현재 스테이지 도달한 사용자)
+            else{  
+                fails.put(i, chellenger[i] / total);
+                //다음 스테이지 실패율을 구하기 위해 현재 스테이지 인원 제외
+                total -= chellenger[i];
             }
-        }        
-        return fials.entrySet().stream().sorted((o1,o2) -> Double.compare(o2.getValue(), o1.getValue())).mapToInt(HashMap.Entry :: getKey).toArray();
+        }
+        // 1. entrySet()으로 HashMap의 Entry Set을 가져옴 (Set<Map.Entry<Integer,Double>>)
+        // 2. stream()으로 스트림 생성
+        // 3. sorted()로 값 기준 내림차순 정렬
+        // 4. mapToInt()로 각 Entry의 키를 IntStream으로 변환
+        // 5. toArray()로 int[] 배열 반환
+        
+        return fails.entrySet().stream().sorted((o1,o2) -> Double.compare(o2.getValue(),o1.getValue())).mapToInt(HashMap.Entry :: getKey).toArray();
     }
 }

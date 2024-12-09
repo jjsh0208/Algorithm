1b507ef8a5a00a61d649751d0d9aac57b57cdef2
DDong_kka <jjsh0208@gmail.com>
2024-12-09 21:25:23 +0900

    [level 2] Title: 괄호 회전하기, Time: 39.48 ms, Memory: 84 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/README.md
+++ 프로그래머스/2/76502. 괄호 회전하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 괄호 회전하기 - 76502 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/76502?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/76502) 
 
 ### 성능 요약
 
-메모리: 82.2 MB, 시간: 35.45 ms
+메모리: 84 MB, 시간: 39.48 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 25일 23:26:32
+2024년 12월 09일 21:25:22
 
 ### 문제 설명
 
--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -1,36 +1,44 @@
 import java.util.HashMap;
-import java.util.ArrayDeque;
+import java.util.Stack;
 
 class Solution {
     public int solution(String s) {
-        int answer = 0;
-        int n = s.length();
-        s += s;
         
+        // 괄호를 확인할 HashMap;
         HashMap<Character,Character> map = new HashMap<>();
+        
         map.put(')','(');
         map.put('}','{');
         map.put(']','[');
         
-    
-        A:for(int i = 0 ; i < n; i++){
-            ArrayDeque<Character> stack = new ArrayDeque<>();
+        int n = s.length(); // 원본 문자열의 길이 저장
+        s += s; // 원본 문자열을 이어 붙여서 2번 나오도록한다.
+        int answer = 0; // 결과값
+        
+        // 확인할 문자열의 시작 인덱스를 0부터 n(문자열 길이) 까지 이동
+        A:for(int i = 0; i < n; i++){
+            Stack<Character> stack = new Stack<>();
             
-            // i(시작위치) 부터 원본 문자열의 길이인 n개까지 올바른 괄호 문자열인지 확인
-            for(int j = i ; j < i + n; j++){
+            for(int j = i; j < i + n; j++){
                 char c = s.charAt(j);
-                 if(!map.containsKey(c)){
-                        stack.push(c);
+                
+                // 해시맵 안에 해당 키가 없다면 여는 괄호다
+                // push 해준다.
+                if(!map.containsKey(c)){
+                    stack.push(c);
                 }else{
+                    // 해시맵 안에 존재하면 닫는 괄호다
+                    // 스택이 비었거나 짝지 맞지않으면 내부 for문 종료하고 for문 A로 이동한다.
                     if(stack.isEmpty() || !stack.pop().equals(map.get(c)))
                         continue A;
                 }
             }
-           
-            if(stack.isEmpty()) answer++;
             
-            
+            if(stack.isEmpty()){
+                answer++;
+            }
         }
+        
         
         return answer;
     }

45f229baff485ecc8bdd3483bc8bf6f34d5900b0
DDong_kka <jjsh0208@gmail.com>
2025-03-28 10:27:42 +0900

    [level 1] Title: 모의고사, Time: 17.75 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -4,34 +4,30 @@ import java.util.Arrays;
 class Solution {
     public int[] solution(int[] answers) {
         
-        // 수포자들의 패턴
-        int[][] pettern = {
-            {1,2,3,4,5},
-            {2,1,2,3,2,4,2,5},
-            {3,3,1,1,2,2,4,4,5,5}
-        };
+       int[][] pattern =  {
+           {1,2,3,4,5},
+           {2, 1, 2, 3, 2, 4, 2, 5},
+           { 3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
+       };
         
-        // 수포자들의 점수를 저장할 배열포자들의 점수를 저장할 배열
         int[] scores = new int[3];
         
-        for(int i = 0 ; i < answers.length; i++){
-            for(int j = 0; j < pettern.length; j++){
-                if(answers[i] == pettern[j][ i % pettern[j].length])
+        for(int i = 0; i < answers.length; i++){
+            for(int j = 0; j < pattern.length; j++){
+                // 어떤 길이가 들어와도 동일하게 처리 가능
+                if(answers[i] == pattern[j][i % pattern[j].length])
                     scores[j]++;
             }
-        } 
+        }
         
-        //가장 높은 점수 저장
         int maxScore = Arrays.stream(scores).max().getAsInt();
         
         ArrayList<Integer> answer = new ArrayList<>();
-        
         for(int i = 0; i < scores.length; i++){
             if(maxScore == scores[i]){
                 answer.add(i + 1);
             }
         }
-        
         
         return answer.stream().mapToInt(Integer :: intValue).toArray();
     }

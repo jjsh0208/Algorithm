50872b76f464a19ebc38c7b543cbd18b813d67c5
DDong_kka <jjsh0208@gmail.com>
2025-04-14 10:39:34 +0900

    [level 1] Title: 카드 뭉치, Time: 0.72 ms, Memory: 87.6 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -1,29 +1,35 @@
-class Solution {
-    
-    // 원하는 카드 뭉치에서 카드를 순서대로 한장씩 사용
-    // 한번 사용한 카드 다시 사용 불가
-    // 카드를 사용하지않고 넘어가지못함
-    // 카드 뭉치의 순서를 바꿀 수 없음
+import java.util.ArrayDeque;
+import java.util.Arrays;
 
+class Solution {
     public String solution(String[] cards1, String[] cards2, String[] goal) {
-       
-        int c1 = 0;
-        int c2 = 0;
         
-        for(String g : goal){
-             if(c1 < cards1.length && cards1[c1].equals(g)){
-                 c1++;
-             }else if(c2 < cards2.length && cards2[c2].equals(g)){
-                 c2++;
-             }else{
-                 return "No";
-             }
+        ArrayDeque<String> card1Deque = new ArrayDeque<>(Arrays.asList(cards1));
+        ArrayDeque<String> card2Deque = new ArrayDeque<>(Arrays.asList(cards2));
+        ArrayDeque<String> goalDeque = new ArrayDeque<>(Arrays.asList(goal));
+        
+        // goal큐가 비어있지않는 동안 반복
+        while(!goalDeque.isEmpty()){
+            
+            //
+            if(!card1Deque.isEmpty() && card1Deque.peekFirst().equals(goalDeque.peekFirst())){
+                goalDeque.pollFirst();
+                card1Deque.pollFirst();
+            }
+            else if(!card2Deque.isEmpty() && card2Deque.peekFirst().equals(goalDeque.peekFirst())){
+                goalDeque.pollFirst();
+                card2Deque.pollFirst();
+            }
+            else {
+                break;
+            } 
         }
         
-        return "Yes";
+        if(goalDeque.isEmpty()){
+            return "Yes";
+        } else{
+            return "No";
+        }
+
     }
-    
-    
-    
-    
 }

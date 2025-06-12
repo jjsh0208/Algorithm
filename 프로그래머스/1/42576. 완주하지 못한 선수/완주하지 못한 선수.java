89781405ff3b398728734dd8961388d17e17e32c
DDong_kka <jjsh0208@gmail.com>
2025-06-12 19:30:50 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 86.84 ms, Memory: 96.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        for(String s : participant){
+             map.put(s, map.getOrDefault(s, 0) + 1);
+        }
+        
+        for(String s : completion){
+             map.put(s , map.get(s) - 1);
+        }
+        
+         for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            if (entry.getValue() != 0) {
+                return entry.getKey();  // 완주 못한 사람 반환
+            }
+        }
+        
+        return null;
+    }
+}

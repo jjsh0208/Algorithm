0b1deffb057fd09049003d1e701658d89d6fbd95
DDong_kka <jjsh0208@gmail.com>
2025-08-07 10:08:03 +0900

    [Silver IV] Title: 카드2, Time: 164 ms, Memory: 45496 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,36 +1,33 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class Main {
+public class Main {    
+
+    static int n;
+    static Queue<Integer> queue;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
-        Queue<Integer> queue = new LinkedList<>();
 
-        int n = Integer.parseInt(br.readLine()); 
+        queue = new LinkedList<>();
+        n = Integer.parseInt(br.readLine());
 
-        for (int i = 1; i <= n; i++) {
-            queue.offer(i);
+        for(int i = 0; i < n ; i++){
+            queue.add(i);
         }
- 
+
         while(queue.size() != 1){
-            queue.remove();
-            int x = queue.poll();
-            queue.offer(x);
+            queue.poll();
+            queue.add(queue.poll());
         }
 
-        bw.write(String.valueOf(queue.poll()));
+        System.out.println(queue.poll() + 1);
 
-        bw.flush();
-        bw.close();
         br.close();
 
     }
 
-}
+} 

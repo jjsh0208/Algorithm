24f52b7e2233c9a648fd360ce410187165b3f021
jjsh0208 <jjsh0208@gmail.com>
2026-02-02 13:38:48 +0900

    [Gold IV] Title: 좋다, Time: 152 ms, Memory: 14816 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.java
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -4,45 +4,41 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main {    
-
-    static int n , i, j , answer;
-    static long[] arr;
+public class Main {
 
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        n = Integer.parseInt(br.readLine());
-        arr = new long[n];
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(br.readLine());
 
+        int[] A = new int[n];
+        int answer = 0;
+
+        StringTokenizer st=  new StringTokenizer(br.readLine());
         for(int i = 0; i < n; i++){
-            arr[i] = Integer.parseInt(st.nextToken());
+            A[i] = Integer.parseInt(st.nextToken());
         }
 
-        Arrays.sort(arr);
+        Arrays.sort(A);
 
-        for(int k = 0; k < n; k++){
-            long target = arr[k];
-            int i = 0;
-            int j = n - 1;
+        for(int a = 0; a < n; a++){
+            int find = A[a]; // 비교할 수
+
+            int i = 0; // 인덱스 시작점
+            int j = n - 1; // 인덱스 마지막
 
             while(i < j){
-                if(arr[i] + arr[j] == target){
-
-                    if(i != k && j != k){
+                if(A[i] + A[j] == find){ // 두수의 합이 찾을 수와 같은 경우
+                    if(i != a && j != a){ // 인덱스가 찾을 수 의 인덱스인지 검사 후 다르면 카운트 증가
                         answer++;
                         break;
-                    }
-                    else if(i == k){
+                    } else if(i == a){ // 앞포인터의 인덱스가 찾을 수와 같으면 앞포인트 증가
                         i++;
-                    }
-                    else if(j == k){
+                    } else if(j == a){ // 뒤포인터의 인덱스가 찾을 수와 같으면 뒤 포인트 감소
                         j--;
                     }
-                }
-                else if(arr[i] + arr[j] < target){
+                }else if(A[i] + A[j] < find){
                     i++;
                 }else{
                     j--;
@@ -51,8 +47,6 @@ public class Main {
         }
 
         System.out.println(answer);
-
-
-
+        br.close();
     }
 }

735312e78b3b26d20d1443415223079ef8ddc88a
jjsh0208 <jjsh0208@gmail.com>
2026-02-02 13:04:00 +0900

    [Silver IV] Title: 주몽, Time: 172 ms, Memory: 16396 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.java
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -4,44 +4,42 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main {    
-
-    static int n , m , i, j , answer;
-    static int arr[];
+public class Main {
 
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        n = Integer.parseInt(br.readLine());
-        m = Integer.parseInt(br.readLine());
 
-        arr = new int[n];
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
 
+        int[] arr = new int[n];
+
+        StringTokenizer st= new StringTokenizer(br.readLine());
         for(int i = 0; i < n; i++){
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
         Arrays.sort(arr);
-        answer = 0;
-        i = 0;
-        j = n - 1;
 
+        int answer = 0;
+        int i = 0;
+        int j = n - 1; // 인덱스는 0부터 시작해서 n보다 -1까지
 
         while(i < j){
-            if(m > arr[i] + arr[j]){
+            if((arr[i] + arr[j]) < m){ // 합이 작으면 앞 포인터 전진
                 i++;
-            }
-            else if(m < arr[i] + arr[j]){
+            }else if((arr[i] + arr[j]) > m){ // 합이 더 크면 뒤 포인트 후진
                 j--;
-            }else{
+            }else if((arr[i] + arr[j]) == m){ // 동일할 시 다음 경우를 위해 앞 포인터 전진, 뒤 포인터 후진하고 카운터증가
+                i++;
+                j--;
                 answer++;
-                i++;
-                j--;
             }
         }
 
         System.out.println(answer);
+
+
     }
 }

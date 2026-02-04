446a01e3afe4ace49c0101c12bbfd8db077f90fa
jjsh0208 <jjsh0208@gmail.com>
2026-02-04 11:36:50 +0900

    [Silver I] Title: 절댓값 힙, Time: 396 ms, Memory: 26260 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -1,39 +1,42 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
+import java.util.LinkedList;
 import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
 
-		PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(((o1, o2) -> {
-			int absO1 = Math.abs(o1);
-			int absO2 = Math.abs(o2);
+    public static void main (String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-			if (absO1 == absO2){
-				return o1 - o2; //절대값이 같으면 원래값이 더 작은 것 부터
-			}else{
-				return absO1 - absO2; // 절대값 기준 정렬
-			}
-		}));
+        int n = Integer.parseInt(br.readLine());
 
-		for (int i = 0; i < n; i++) {
-			int x = Integer.parseInt(br.readLine());
+        //PriorityQueue
+        PriorityQueue<Integer> queue = new PriorityQueue<>((o1,o2) ->{
+            int first = Math.abs(o1);
+            int second = Math.abs(o2);
 
-			if (x == 0){
-				if (!priorityQueue.isEmpty()){
-					System.out.println(priorityQueue.poll());
-				}else{
-					System.out.println(0);
-				}
-			}else{
-				priorityQueue.offer(x);
-			}
-		}
+            if(first == second){
+                return o1 > o2 ? 1 : -1; //절대값이 같으면 음수 우선으로 정렬
+            }else{
+                return first - second; // 절대값이 다르면 절대값 기준으로 정렬
+            }
+        });
 
-		br.close();
-	}
+
+        for(int i = 0; i < n; i++){
+            int num = Integer.parseInt(br.readLine());
+            if(num == 0){
+                if(queue.isEmpty()){
+                    System.out.println(0);
+                }else{
+                    System.out.println(queue.poll());
+                }
+            }else{
+                queue.offer(num);
+            }
+        }
+        br.close();
+    }
 }

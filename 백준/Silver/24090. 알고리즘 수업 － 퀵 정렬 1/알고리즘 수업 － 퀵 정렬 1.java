552d32fe9efd8d2afbbe11a551b8438ae2ac25ec
jjsh0208 <jjsh0208@gmail.com>
2026-02-09 11:42:28 +0900

    [Silver V] Title: 알고리즘 수업 - 퀵 정렬 1, Time: 280 ms, Memory: 18572 KB -BaekjoonHub

--- 백준/Silver/24090. 알고리즘 수업 － 퀵 정렬 1/알고리즘 수업 － 퀵 정렬 1.java
+++ 백준/Silver/24090. 알고리즘 수업 － 퀵 정렬 1/알고리즘 수업 － 퀵 정렬 1.java
@@ -1,77 +1,75 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main {    
+public class Main {
 
-    static int n , k , cnt;
-    static int[] arr;
+    static int n,k,cnt;
 
-    public static void main(String[] args) throws IOException {
+    public static void main (String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st= new StringTokenizer(br.readLine());
 
-        n = Integer.parseInt(st.nextToken());
-        k = Integer.parseInt(st.nextToken());
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        arr = new int[n];
+        n = Integer.parseInt(st.nextToken()); // 배열의 크기
+        k = Integer.parseInt(st.nextToken()); // 교환 횟수
 
-        st= new StringTokenizer(br.readLine());
-        
+        int[] arr = new int[n];
+        cnt = 0;
+
+
+        st = new StringTokenizer(br.readLine());
         for(int i = 0; i < n; i++){
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        quickSort(arr, 0 , n - 1);
+        quickSort(arr,0, n - 1);
 
-        if (cnt < k) {
-            System.out.print("-1");
+        if(cnt < k){
+            System.out.println(-1);
         }
 
-        br.close(); 
-    }
-
-
-    private static void quickSort(int[] A , int start, int end){
-        if(start < end){
-            int pivot = partition(A,start,end);
-            quickSort(A, start , pivot - 1); // 왼쪽 분리 집합 정렬 
-            quickSort(A, pivot + 1, end); // 오른쪽 분리 집합 정렬
-
-        }
-    }
-
-    private static int partition(int[] A , int start , int end){
-        int pivot = A[end];
-        int idx = start - 1;
-
-        for(int i = start;  i < end; i++){
-            if(A[i] <= pivot){
-                swap(A, ++idx, i);
-            }
-
-        }
-
-        if(idx + 1 != end){
-            swap(A, idx + 1, end);
-        }
-
-        return idx + 1;
 
     }
 
-    private static void swap(int[] A , int i, int j){
-        int temp = A[i];
-        A[i] = A[j];
-        A[j] = temp;
+    private static void quickSort(int[] arr, int s, int e) {
+        if(s < e){
+            int pivot = partition(arr,s,e); // 분할 시작
+            quickSort(arr, s, pivot - 1);
+            quickSort(arr, pivot + 1, e);
+        }
+    }
+
+    
+    private static int partition(int[] arr, int s, int e) {
+        int x = arr[e];
+        int index = s - 1;
+
+        for(int i = s; i < e; i++){
+             if(arr[i] < x){
+                 swap(arr, ++index, i);
+             }
+        }
+
+        if(index + 1 != e){
+            swap(arr, index + 1, e);
+        }
+
+        return index + 1;
+
+    }
+
+    private static void swap(int[] arr, int s, int e) {
+        int temp = arr[s];
+        arr[s] = arr[e];
+        arr[e] = temp;
 
         cnt++;
 
         if(cnt == k){
-            System.out.println(A[i] + " " + A[j]);
+            System.out.println(arr[s]+ " " + arr[e]);
         }
     }
-
-
-} 
+}

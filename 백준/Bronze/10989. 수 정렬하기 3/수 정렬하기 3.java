44bde50a4cded9af5b5b8408b5cd55439f9390e0
jjsh0208 <jjsh0208@gmail.com>
2026-02-24 11:49:48 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 2320 ms, Memory: 339296 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -1,65 +1,59 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
+import java.util.*;
 
-public class Main {    
+public class Main {
 
-    public static int[] A;
-    public static long result;
-    
-    public static void main(String[] args) throws IOException {
+    public static void main (String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int N = Integer.parseInt(br.readLine());
-        A = new int[N];
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
 
-        for(int i = 0; i < N; i++){
-            A[i] = Integer.parseInt(br.readLine());
+        for(int i = 0; i < n; i++){
+            arr[i] = Integer.parseInt(br.readLine());
         }
         br.close();
 
-        // 5를 주는 이유는 최대 5자리 수 의 수 까지만 받기 때문
-        RadixSort(A,5);
+        RadixSort(arr,5); //배열과 최대 자릿수
 
-        for(int i = 0; i < N; i++){
-            bw.write(A[i] + "\n");
+        for(int i = 0 ; i < n; i++){
+            bw.write(arr[i] + "\n");
         }
         bw.flush();
         bw.close();
+
     }
 
-    private static void RadixSort(int[] A, int maxSize){
-        int[] output = new int[A.length]; // 임시 정렬 배열
-        int jarisu = 1; // 현재 자리 수
+    private static void RadixSort(int[] arr, int maxSize) {
+        int[] output = new int[arr.length];
+        int jarisu = 1;
         int cnt = 0;
 
-        // 최대 자리 수 만큼 반복하기
         while(cnt != maxSize){
-            int[] bucket = new int[10];
 
-            for(int i = 0; i < A.length; i++){
-                bucket[ (A[i] / jarisu )% 10]++;
+            int[] bucket = new int[10]; // 1부터 10의 자리수를 카운터할 배열 , 자리수 분포를 합배열로 확인할 배열
+
+            for(int i = 0; i < arr.length; i++){
+                bucket[(arr[i] / jarisu) % 10]++;
             }
 
-            for(int i = 1; i < 10; i++){
+            for(int i = 1; i < 10; i++){ // 합배열을 사용해 인덱스 계산
                 bucket[i] += bucket[i - 1];
             }
 
-            for(int i = A.length - 1; i >= 0; i--){
-                output[bucket[A[i] / jarisu % 10] - 1] = A[i];
-                bucket[(A[i] / jarisu) % 10]--;
+            for(int i = arr.length - 1; i >= 0; i--){
+                output[bucket[(arr[i] / jarisu % 10)] - 1] = arr[i];
+                bucket[(arr[i] / jarisu) % 10]--;
             }
 
-            for(int i = 0; i < A.length; i++){
-                A[i] = output[i];
+            for(int i = 0; i < arr.length; i++){
+                arr[i] = output[i];
             }
 
-            jarisu *= 10;
+            jarisu *= 10; // 자리수 증가시키기
             cnt++;
-
         }
+
     }
 }

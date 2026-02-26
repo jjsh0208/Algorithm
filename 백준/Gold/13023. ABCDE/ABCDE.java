2d8d2658e29f2eaed9804f9388758a6cacb5f3f8
jjsh0208 <jjsh0208@gmail.com>
2026-02-26 11:40:23 +0900

    [Gold V] Title: ABCDE, Time: 256 ms, Memory: 22224 KB -BaekjoonHub

--- 백준/Gold/13023. ABCDE/ABCDE.java
+++ 백준/Gold/13023. ABCDE/ABCDE.java
@@ -1,70 +1,61 @@
+import java.io.*;
 import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    static int N , M; // 노드(사람의 수) 와 간선(친구 관계의 수)
-    static ArrayList<Integer>[] A;// 인접 리스트 그래프 (각 노드와 연결된 노드 목록)
-    static boolean[] visited; // 방문 확인용 변수
-    static boolean check; // 깊이 5(친구 관계 5명 연속) 발견 여부
+    static int n,m;
+    static ArrayList<Integer>[] arr;
+    static boolean check;
+    static boolean[] visited;
 
-    public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt(); // 노드 입력
-        M = sc.nextInt(); // 간선 입력
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        // 변수 초기화
-        A = new ArrayList[N];
-        visited = new boolean[N];
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new ArrayList[n];
+        visited = new boolean[n];
         check = false;
 
-        // ArrayList 초기화
-        for(int i = 0; i < N; i++){
-            A[i] = new ArrayList<Integer>();
+        for(int i = 0; i < n; i++){
+            arr[i] = new ArrayList<>();
         }
 
-        //각 노드 연결
-        for(int i = 0; i < M; i++){
-            int s = sc.nextInt();
-            int e = sc.nextInt();
+        for(int i = 0; i < m; i++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
 
-            A[s].add(e);
-            A[e].add(s);
+            arr[a].add(b);
+            arr[b].add(a);
         }
 
-        // 모든 노드를 시작점으로 DFS 시작
-        for(int i = 0; i < N; i++){
-            // 0부터 N까지의 수 중에서
-            DFS(i, 1);
 
-            // 길이가 5인 경로(친구 5명 연속 연결) 발견 시 종료
+        for(int i = 0; i < n; i++){
+            DFS(i,1);
+
             if(check) break;
         }
 
-        // 결과 유무에 따른 1과 0 출력
-        System.out.println(check ? 1 : 0);
+        if(check) System.out.println(1);
+        else System.out.println(0);
     }
 
-    private static void DFS(int num, int depth){
-        // 현재 깊이가 5이거나 도착 확인 변수가 true이면 종료
-        if (depth == 5) {  
-            check = true;  
-            return;  
-        }  
+    private static void DFS(int v, int depth) {
+        if(depth == 5 || check){
+            check = true;
+            return;
+        }
+        visited[v] = true;
 
-        if (check) return;  
-
-        // 현재 위치 방문
-        visited[num] = true;
-
-        // 인접 노드 중 방문하지 않은 노드로 DFS (깊이 +1)
-        for(int i : A[num]){
+        for(int i : arr[v]){
             if(!visited[i]){
                 DFS(i, depth + 1);
             }
         }
-
-        // 순회가 끝나면 다시 방문 초기화
-        visited[num] = false;
+        visited[v] = false;
     }
 }

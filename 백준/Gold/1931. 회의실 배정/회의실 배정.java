8b92b80dbabd335bc0558d1ed197d9e9cdf9f9bd
jjsh0208 <jjsh0208@gmail.com>
2026-03-07 12:42:47 +0900

    [Gold V] Title: 회의실 배정, Time: 1044 ms, Memory: 181608 KB -BaekjoonHub

---
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[][] arr = new int[n][2];
+
+        for(int i = 0; i < n; i++){
+            arr[i][0] = sc.nextInt();
+            arr[i][1] = sc.nextInt();
+        }
+
+        Arrays.sort(arr, new Comparator<int[]>(){
+
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if(o1[1] == o2[1]){
+                    return o1[0] - o2[0];
+                }
+                return o1[1] - o2[1];
+            }
+        });
+
+        int cnt = 0;
+        int end = -1;
+
+
+        for(int i = 0; i < n; i++){
+            if(arr[i][0] >= end){
+                end = arr[i][1];
+                cnt++;
+            }
+        }
+
+        System.out.println(cnt);
+    }
+
+
+}
+
+
+
+
+

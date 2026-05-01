4d975ebf38ea41a5ebd68935ee19dd84d8f6930b
jjsh0208 <jjsh0208@gmail.com>
2026-05-01 12:34:33 +0900

    [level 3] Title: 네트워크, Time: 0.29 ms, Memory: 86 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,29 +1,32 @@
 class Solution {
     
+
     boolean[] visited;
-    
     public int solution(int n, int[][] computers) {
-        
-        visited = new boolean[n]; // 몇번 컴퓨터에 접근했는지 확인
-        
         int answer = 0;
+        visited = new boolean[n];
+        
         for(int i = 0; i < n; i++){
             if(!visited[i]){
+                dfs(i, computers);    
                 answer++;
-                DFS(n,computers,i);
             }
         }
-        
         return answer;
     }
     
-    private void DFS(int n, int[][] comp, int index){
-        visited[index] = true;
+    
+    private int dfs(int idx , int[][]computers){
+         
+       visited[idx] = true;
         
-        for(int i = 0; i < n; i++){
-            if(comp[index][i] == 1 && !visited[i]){
-                DFS(n,comp,i);
-            }
+        for(int i = 0; i < computers.length; i++){
+           if(idx != i && !visited[i] && computers[i][idx] == 1){
+               dfs(i,computers);
+           }
+            
         }
+        return 0;
     }
+    
 }

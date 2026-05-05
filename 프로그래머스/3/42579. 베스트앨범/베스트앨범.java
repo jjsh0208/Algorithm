ec9ac895a95f83a488370c56d5b6d53dd5170a9b
jjsh0208 <jjsh0208@gmail.com>
2026-05-05 14:19:04 +0900

    [level 3] Title: 베스트앨범, Time: 3.74 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -2,61 +2,68 @@ import java.util.HashMap;
 import java.util.ArrayList;
 import java.util.Collections;
 
+
 class Solution {
-    
-    //속한 노래가 많이 재생된 장르를 먼저 수록합니다.
-    //장르 내에서 많이 재생된 노래를 먼저 수록합니다.
-    //장르 내에서 재생 횟수가 같은 노래 중에서는 고유 번호가 낮은 노래를 먼저 수록합니다.
-  
     public int[] solution(String[] genres, int[] plays) {
-        HashMap<String,Integer> totalPlayMap = new HashMap<>();
-        HashMap<String,ArrayList<Song>> songMap = new HashMap<>();
+        // 1. 많이 재생된 장르
+        // 2. 장르 내에서 많이 재생된 노래를 먼저 수록
+        // 3. 장르 내에서 재생 횟수가 같으면 고유 번호가 낮은 순으로 처리 (index)
+        // 4. 장르별 노래는 두개씩이다.
+        
+        HashMap<String,Integer> totalPlay = new HashMap<>(); // 각 장르별 총 재생 횟수
+        HashMap<String,ArrayList<Song>> songMap = new HashMap<>(); // 각 장르별 장르 등장 횟수
         
         for(int i = 0; i < genres.length; i++){
             String genre = genres[i];
-            int playTime = plays[i];
+            int play = plays[i];
             
-            totalPlayMap.put(genre, totalPlayMap.getOrDefault(genre , 0) + playTime);
+            totalPlay.put(genre, totalPlay.getOrDefault(genre,0) + play);
             
-            songMap.putIfAbsent(genre, new ArrayList<>());
-            songMap.get(genre).add(new Song(i,playTime));
+            songMap.putIfAbsent(genre,new ArrayList<>());
+            songMap.get(genre).add(new Song(i,play));
         }
         
-        ArrayList<String> genrList = new ArrayList<>(totalPlayMap.keySet());
-        Collections.sort(genrList, (o1,o2) -> totalPlayMap.get(o2) - totalPlayMap.get(o1));
         
+        ArrayList<String> genrList = new ArrayList<>(totalPlay.keySet());
         
-        ArrayList<Integer> result = new ArrayList<>();
-        for(String key : genrList){
+        Collections.sort(genrList,(o1,o2) -> totalPlay.get(o2) - totalPlay.get(o1));
+        
+        ArrayList<Integer> answer = new ArrayList<>();
+        
+        for(String grenr : genrList){
+            // 장르는 플레이타임 기준으로 내림차순 되어있음 각 장르별 노래 두개 씩 꺼내됨
             
-            ArrayList<Song> songList = songMap.get(key);
+            ArrayList<Song> songList = songMap.get(grenr);
             
             Collections.sort(songList);
             
-            int limit = Math.min(songList.size(), 2);
+            int rimit = Math.min(songList.size(),2);
             
-            for(int i = 0; i < limit; i++){
-                result.add(songList.get(i).id);
-            }
-        }    
-        
-        return result.stream().mapToInt( i -> i).toArray();    
+            for(int i = 0; i < rimit; i++){
+                answer.add(songList.get(i).id);
+            }   
+        }
+        return answer.stream().mapToInt(i -> i).toArray();
     }
+    
     
     class Song implements Comparable<Song>{
         int id;
-        int play;
+        int playTime;
         
-        public Song(int id, int play){
+        public Song(int id, int playTime){
             this.id = id;
-            this.play = play;
+            this.playTime = playTime;
         }
         
-        public int compareTo(Song other) {
-            if (this.play == other.play) {
+        @Override
+        public int compareTo(Song other){
+            if(this.playTime == other.playTime){
                 return this.id - other.id;
             }
-            return other.play - this.play;
+            
+            return other.playTime - this.playTime;
         }
+        
     }
 }
